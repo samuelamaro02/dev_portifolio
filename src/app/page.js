515a1d2f7dc32bfd81d0389ed573d1addf62819b
samuelamaro02 <134892999+samuelamaro02@users.noreply.github.com>
2025-03-26 22:48:17 +0000

    adiciona section Skils, components e pasta images e icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,11 @@
-import styles from "./page.module.css";
+import Hero from "./components/hero";
+import SectionSkills from "./components/sectionSkills";
 
 export default function Home() {
   return (
-    <div className={styles.container}> 
-      <div className={styles.heroContent}>
-        <div>
-          <h1>
-            <span className={styles.accentText}>Oi, eu sou</span>{' '}
-            Samuel Amaro.
-            {' '}<span className={styles.accentText}>Dev Front-end & Webdesigner.</span>{' '}
-          </h1>
-          <p>Transformo linhas de código em experiências fluidas e pixels em narrativas visuais. 
-            Apaixonado por criar interfaces que não só funcionam, mas encantam. 
-            <span className={styles.emphasisText}>Front-end é minha linguagem, mas a criatividade é meu framework.</span>
-          </p>
-          <a href="https://wa.me/5583998404188">Entrar em contato</a>
-        </div>
-        <div>
-          <img 
-              src="/hero.svg"  
-              className={styles.heroImage}
-            />
-          <img 
-              src="/heroMobile.svg"  
-              className={styles.heroImageMobile}
-            />
-        </div>
-      </div>
-    </div>
+    <main>
+      <Hero/>
+      <SectionSkills/>
+    </main>
   );
 }
