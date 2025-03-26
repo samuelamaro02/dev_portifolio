69c4eece8b0c7df74bfa6f368302294b2ffc0a07
samuelamaro02 <134892999+samuelamaro02@users.noreply.github.com>
2025-03-26 00:21:34 +0000

    Sua mensagem de commit aqui

---
+++ src/app/layout.js
@@ -0,0 +1,23 @@
+import { Sora } from "next/font/google";
+import "./globals.css";
+
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["300", "400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "Samuel Amaro",
+  description: "Samuel Amaro - Webdesigner e Dev",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${sora.variable}`}>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
