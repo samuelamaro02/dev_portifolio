9febd44270b1ed222f48e3e4ef7a3f23658661d8
samuelamaro02 <134892999+samuelamaro02@users.noreply.github.com>
2025-03-27 15:24:43 +0000

    adiciona next/image

--- src/app/components/sectionExperience.js
+++ src/app/components/sectionExperience.js
@@ -1,51 +1,61 @@
+import Image from 'next/image';
 import styles from "./sectionExperience.module.css"; 
 
-export default function sectionExperience(){
+export default function SectionExperience() {
     return (
-        <>
-        
         <div className={styles.container}>
             <div className={styles.containerExperience}>
                 <h1>My Experience</h1>
                 <div className={styles.gridExperience}>
                     <div className={styles.cardExperience}>
                         <div className={styles.cardTittle}>
-                            <img 
+                            <Image 
                                 src="/icons/google.svg"  
-                                alt=""
+                                alt="Google logo"
+                                width={40}
+                                height={40}
                             />
                             <h4>Lead Software Engineer at Google</h4>
                             <h6>Nov 2019 - Present</h6>
                         </div>
-                        <div className={styles.cardDescription}><p>As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google's core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.</p></div>
+                        <div className={styles.cardDescription}>
+                            <p>As a Senior Software Engineer at Google, I played a pivotal role in developing innovative solutions for Google&apos;s core search algorithms. Collaborating with a dynamic team of engineers, I contributed to the enhancement of search accuracy and efficiency, optimizing user experiences for millions of users worldwide.</p>
+                        </div>
                     </div>
+
                     <div className={styles.cardExperience}>
                         <div className={styles.cardTittle}>
-                            <img 
+                            <Image 
                                 src="/icons/youtube.svg"  
-                                alt=""
+                                alt="YouTube logo"
+                                width={40}
+                                height={40}
                             />
-                            <h4>Software Engineer at Youtube</h4>
+                            <h4>Software Engineer at YouTube</h4>
                             <h6>Jan 2017 - Oct 2019</h6>
                         </div>
-                        <div className={styles.cardDescription}><p>At Youtube, I served as a  Software Engineer, focusing on the design and implementation of backend systems for the social media giant's dynamic platform. Working on projects that involved large-scale data processing and user engagement features, I leveraged my expertise to ensure seamless functionality and scalability.</p></div>
+                        <div className={styles.cardDescription}>
+                            <p>At YouTube, I served as a Software Engineer, focusing on the design and implementation of backend systems for the social media giant&apos;s dynamic platform. Working on projects that involved large-scale data processing and user engagement features, I leveraged my expertise to ensure seamless functionality and scalability.</p>
+                        </div>
                     </div>
+
                     <div className={styles.cardExperience}>
                         <div className={styles.cardTittle}>
-                            <img 
+                            <Image 
                                 src="/icons/apple.svg"  
-                                alt=""
+                                alt="Apple logo"
+                                width={40}
+                                height={40}
                             />
                             <h4>Junior Software Engineer at Apple</h4>
                             <h6>Jan 2016 - Dec 2017</h6>
                         </div>
-                        <div className={styles.cardDescription}><p>During my tenure at Apple, I held the role of Software Architect, where I played a key role in shaping the architecture of mission-critical software projects. Responsible for designing scalable and efficient systems, I provided technical leadership to a cross-functional team.</p></div>
+                        <div className={styles.cardDescription}>
+                            <p>During my tenure at Apple, I held the role of Software Architect, where I played a key role in shaping the architecture of mission-critical software projects. Responsible for designing scalable and efficient systems, I provided technical leadership to a cross-functional team.</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        
-        </>
-    )
-
+    );
 }
