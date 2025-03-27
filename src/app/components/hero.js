f44a21077a26269ef9ae38435b9d5e0e0673c9ef
samuelamaro02 <134892999+samuelamaro02@users.noreply.github.com>
2025-03-27 16:49:50 +0000

    corrige image hero e apóstrofo about

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,34 +1,44 @@
+import Image from "next/image";
 import styles from "./hero.module.css";
 
 export default function Home() {
   return (
     <>
-    <div className={styles.container}> 
-      <div className={styles.heroContent}>
-        <div>
-          <h1>
-            <span className={styles.accentText}>Oi, eu sou</span>{' '}
-            Samuel Amaro.
-            {' '}<span className={styles.accentText}>Dev Front-end & Webdesigner.</span>{' '}
-          </h1>
-          <p>Transformo linhas de código em experiências fluidas e pixels em narrativas visuais. 
-            Apaixonado por criar interfaces que não só funcionam, mas encantam. 
-            <span className={styles.emphasisText}>Front-end é minha linguagem, mas a criatividade é meu framework.</span>
-          </p>
-          <a href="https://wa.me/5583998404188">Entrar em contato</a>
-        </div>
-        <div>
-          <img 
+      <div className={styles.container}> 
+        <div className={styles.heroContent}>
+          <div>
+            <h1>
+              <span className={styles.accentText}>Oi, eu sou</span>{' '}
+              Samuel Amaro.
+              {' '}<span className={styles.accentText}>Dev Front-end & Webdesigner.</span>{' '}
+            </h1>
+            <p>
+              Transformo linhas de código em experiências fluidas e pixels em narrativas visuais. 
+              Apaixonado por criar interfaces que não só funcionam, mas encantam. 
+              <span className={styles.emphasisText}>Front-end é minha linguagem, mas a criatividade é meu framework.</span>
+            </p>
+            <a href="https://wa.me/5583998404188">Entrar em contato</a>
+          </div>
+          <div>
+            <Image 
               src="/icons/hero.svg"  
               className={styles.heroImage}
+              alt="Ilustração representando desenvolvimento web"
+              width={500} 
+              height={500} 
+              priority
             />
-          <img 
+            <Image 
               src="/icons/heroMobile.svg"  
               className={styles.heroImageMobile}
+              alt="Ilustração para dispositivos móveis"
+              width={300} 
+              height={300} 
+              priority
             />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- src/app/components/sectionAbout.js
+++ src/app/components/sectionAbout.js
@@ -16,14 +16,13 @@ export default function SectionAbout() {
                 </div>
                 <div className={styles.aboutDescription}>
                     <h1>About Me</h1>
-                    <p>I'm a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me.</p>
+                    <p>I&apos;m a passionate, self-proclaimed designer who specializes in full stack development (React.js & Node.js). I am very enthusiastic about bringing the technical and visual aspects of digital products to life. User experience, pixel perfect design, and writing clear, readable, highly performant code matters to me.</p>
                     <br></br>
-                    <p>I began my journey as a web developer in 2015, and since then, I've continued to grow and evolve as a developer, taking on new challenges and learning the latest technologies along the way. Now, in my early thirties, 7 years after starting my web development journey, I'm building cutting-edge web applications using modern technologies such as Next.js, TypeScript, Nestjs, Tailwindcss, Supabase and much more.</p>
+                    <p>I began my journey as a web developer in 2015, and since then, I&apos;ve continued to grow and evolve as a developer, taking on new challenges and learning the latest technologies along the way. Now, in my early thirties, 7 years after starting my web development journey, I&apos;m building cutting-edge web applications using modern technologies such as Next.js, TypeScript, Nestjs, Tailwindcss, Supabase and much more.</p>
                     <br></br>
-                    <p>When I'm not in full-on developer mode, you can find me hovering around on twitter or on indie hacker, witnessing the journey of early startups or enjoying some free time. You can follow me on Twitter where I share tech-related bites and build in public, or you can follow me on GitHub.</p>
+                    <p>When I&apos;m not in full-on developer mode, you can find me hovering around on twitter or on indie hacker, witnessing the journey of early startups or enjoying some free time. You can follow me on Twitter where I share tech-related bites and build in public, or you can follow me on GitHub.</p>
                 </div>
             </div>
         </div>
-
     )
 }
