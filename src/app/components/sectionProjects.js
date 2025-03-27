cf2589d31382bb3fbf8b73df556819f4deb82455
samuelamaro02 <134892999+samuelamaro02@users.noreply.github.com>
2025-03-27 18:39:10 +0000

    adiciona apóstrofo section projects

--- src/app/components/sectionProjects.js
+++ src/app/components/sectionProjects.js
@@ -18,7 +18,7 @@ export default function SectionProjetcs(){
                             <div className={styles.projectsDescription}>
                                 <h1>01</h1>
                                 <h2>Crypto Screener Application</h2>
-                                <p>I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.</p>
+                                <p>I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.</p>
                             </div>
                     </div>
                     <div className={styles.cardProjects}>
@@ -32,7 +32,7 @@ export default function SectionProjetcs(){
                             <div className={styles.projectsDescription}>
                                 <h1>02</h1>
                                 <h2>Euphoria - Ecommerce (Apparels) Website Template</h2>
-                                <p>I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
+                                <p>I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.
                                 when an unknown printer took a galley of type and scrambled it to specimen book.</p>
                             </div>
                     </div>
@@ -47,7 +47,7 @@ export default function SectionProjetcs(){
                             <div className={styles.projectsDescription}>
                                 <h1>03</h1>
                                 <h2>Blog Website Template</h2>
-                                <p>I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.</p>
+                                <p>I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book.</p>
                             </div>
                     </div>
                 </div>
